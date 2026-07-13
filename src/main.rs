3ad5a6fd362484fd7e185169726868af17c3367b
Yoyosmu <jun.mu57@gmail.com>
2026-07-12 22:42:29 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,25 @@
 use crossterm::event::{poll, read, Event, KeyCode, KeyEventKind};
 use crossterm::{execute, terminal::{disable_raw_mode, enable_raw_mode, size}, style::Print, cursor::{MoveTo, Hide, Show}};
-use std::io::{self, Write};
+use std::io::{self};
 use std::time::Duration;
 
 fn main() -> io::Result<()> {
-    let mut stdout = io::stdout(); 
-    let (groundx, groundy) = draw_ground()?; 
-
-    print!("\x1B[2J\x1B[1;1H");
-    enable_raw_mode()?;
-    execute!(stdout, Hide).unwrap();
-
-    draw_ground()?;
-	
-    run_char(stdout, groundx, groundy)?;
-
-    disable_raw_mode()?;
-    execute!(io::stdout(), Show).unwrap();
-    Ok(())
-}
-
-fn draw_ground() -> io::Result<(u16, u16)> {
-    let mut stdout = io::stdout();
-    let groundy: u16 = 12; 
-    let (width, _) = size()?;
-    
-    for groundx in 0..width {
-        execute!(stdout, MoveTo(groundx, groundy), Print("▔"))?;
-    }
-    
-    let groundx: u16 = width;
-    Ok((groundx, groundy))
-}
-
-fn run_char(mut stdout: io::Stdout, groundx: u16, groundy: u16, ) -> io::Result<()> {    
 	let mut x: u16 = 0;
-    let mut y: u16 = 0;
-    let mut velocityx: i16 = 0;
-    let mut velocityy: i16 = 0;
-    let mut spaces: u16 = 0;
-    let mut spaces2: u16 = 0;
-
+	let mut y: u16 = 0;
+	let mut prev_x: u16 = 0;
+	let mut prev_y: u16 = 0;	
+    print!("\x1B[2J\x1B[1;1H");
+    let mut stdout = io::stdout(); 
+    enable_raw_mode()?;
+	execute!(stdout, Hide).unwrap();
+   		
     loop {
-    	let mut right = false;
-    	let mut left = false;
-    	let mut up = false;
-    	let mut down = false;	
-    			        	
-        if poll(Duration::from_millis(120))? {
+   		let mut right = false;
+   		let mut left = false;
+   		let mut up = false;
+   		let mut down = false;    
+
+		if poll(Duration::from_millis(100))? {
             if let Event::Key(key) = read()? {
                 if key.kind == KeyEventKind::Press {
                 	match key.code {
@@ -55,58 +27,34 @@ fn run_char(mut stdout: io::Stdout, groundx: u16, groundy: u16, ) -> io::Result<
                         KeyCode::Char('a') => left = true,
                         KeyCode::Char('w') => up = true,
                         KeyCode::Char('s') => down = true,
-                        KeyCode::Char('q') => return Ok(()),
+                        KeyCode::Char('q') => {
+                        	execute!(io::stdout(), Show).unwrap();
+                            disable_raw_mode()?;
+                            return Ok(());
+                        },
                         _ => {}
                     }
-                }                    
-
-                draw_ground()?;
-
-
-
-                if right {
-                    x += 1;
-                    velocityx += 2;
-                } else if left {
-                    if x > 0 { x -= 1; };
-                    velocityx -= 2;
                 }
-
-                if down {
-                    if y + 1 == 3 { y += 1; }
-                    velocityy += 2;
-                } else if up && y == (groundy - 1) {
-                    y -= 3; 
-                    velocityy -= 2;
-                }             
-            }          
-        }
-
-        if x > groundx {
-            if y < 50 { y += 1; } 
-        } else if y < (groundy - 1) {
-            velocityy += 1;
-            y += 1;
-        } else {
-            y = groundy - 1;
-            velocityy = 0;  
-        }
-
-        draw_ground()?;
-
-
-        x = (x as i16 + velocityx).max(0) as u16;
-        y = (y as i16 + velocityy).max(0) as u16;
-
-        if x != spaces || y != spaces2 {
-            execute!(stdout, MoveTo(spaces.max(0), spaces2.max(0)), Print(" "))?;
-            execute!(stdout, MoveTo(x.max(0), y.max(0)), Print("i"))?;
-            stdout.flush()?;            
-            spaces = x; 
-            spaces2 = y; 
-        }      
-
-        velocityx = 0;
+            }
+			if right {
+				x += 1;
+			}
+			if left {
+				x = x.saturating_sub(1);
+			}
+			if down {
+				y += 1;
+			}
+			if up {
+				y = y.saturating_sub(1);
+			}
+                
+			if x != prev_x || y != prev_y {
+				execute!(io::stdout(), MoveTo(prev_x.max(0), prev_y.max(0)), Print(" "))?;
+				execute!(io::stdout(), MoveTo(x.max(0), y.max(0)), Print("i"))?;
+				prev_y = y;
+				prev_x = x;
+			}
+    	}	  
     }
-    Ok(())
 }
